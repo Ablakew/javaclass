198c7ac2b8962e92e9bee2e8762ca6267d3e6350
Ablakew <136542715+Ablakew@users.noreply.github.com>
2023-06-17 21:41:02 -0400

    Add files via upload

---
+++ Datatype_Assignment2.js
@@ -0,0 +1,13 @@
+let x= undefined
+console.log(typeof(x))
+let y=NaN
+B=Number*(y)
+console.log(B)
+ let z= 0
+ console.log(typeof(z))
+  let a=false
+  console.log(typeof(a))
+  let n= Kirubel
+  console.log(typeof(n))
+  let i= 123
+  console.log(typeof(i))
